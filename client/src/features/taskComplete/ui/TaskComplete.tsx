a4d1e82d1eec091502e4d6f17dde12d51c6ff2d0
Kalabass <sevagavgav@yandex.ru>
2024-05-10 17:06:57 +0300

    for cors fix

--- client/src/features/taskComplete/ui/TaskComplete.tsx
+++ client/src/features/taskComplete/ui/TaskComplete.tsx
@@ -3,6 +3,7 @@ import FailPath from '@/shared/assets/gifs/failed-failure.gif';
 import loadPath from '@/shared/assets/gifs/loading.gif';
 import SuccessPath from '@/shared/assets/gifs/success.gif';
 import { useQueryParams } from '../helpers/useQueryParams';
+
 export const TaskComplete: React.FC = () => {
 	const task_id = useQueryParams();
 	const updateMutation = useUpdateTask();
--- client/src/pages/main/ui/Page.tsx
+++ client/src/pages/main/ui/Page.tsx
@@ -1,4 +1,8 @@
+import { useProducts } from '@/entities/product';
+
 export const MainPage: React.FC = () => {
+	const { data } = useProducts();
+	console.log(data);
 	return (
 		<>
 			<p>Tamagotchi</p>
