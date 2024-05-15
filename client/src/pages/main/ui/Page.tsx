5209b5de0ae8804984fe3dc98f30009a5fb62a3f
Aziz Madaminov <madaminovaziz@mail.ru>
2024-05-15 19:25:21 +0300

    modified:   client/src/pages/main/ui/Page.tsx 	new file:   client/src/pages/main/ui/style.scss

--- client/src/pages/main/ui/Page.tsx
+++ client/src/pages/main/ui/Page.tsx
@@ -1,12 +1,24 @@
 import { useNavigate } from 'react-router-dom';
+import '../ui/style.scss'
 
 export const MainPage: React.FC = () => {
 	const navigate = useNavigate();
 	return (
-		<>
-			<p>Tamagotchi</p>
-			<img src='src/shared/assets/tama.png' />
-			<button onClick={() => navigate('/main/chat')}>chat!</button>
-		</>
+		<div className="page-wrapper">
+			<div className="main-wrapper">
+				<div className="tamagotchi-wrapper">
+					
+				</div>
+
+
+
+				<button 
+					className='primary-button chat-button' 
+					onClick={() => navigate('/main/chat')	
+				}>
+					Перейти в чат!
+				</button>
+			</div>
+		</div>
 	);
 };
